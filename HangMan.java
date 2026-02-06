a0bde3969fb12f7bc7fa3040aa0830e4d5ebfd29
JesusAlmanza05 <jesusalmanza351@gmail.com>
2026-02-05 19:47:16 -0600

    Add files via upload
    iteration 2

--- HangMan.java
+++ HangMan.java
@@ -13,46 +13,127 @@ public class HangMan {
     //Array list to hold words from word bank
     ArrayList<String> words = new ArrayList<>();
 
+    //Array that holds guesses
+    ArrayList<Character> guesses = new ArrayList<>();
+
+
     //Initialize variables
     String selectedWord=" ";
     boolean winner = false;
+    boolean gameOver = false;
+    int lives=6;
+    char displayWord[];
 
+
+    //Constructor calls methods to get word and display game
     public HangMan() throws Exception{
         getWord();
         displayGame();
     }
 
     //method reads file and adds them to word list, then selects random word from list
-    public void getWord() throws Exception{
-
+    public void getWord() throws Exception
+    {
+        //reads file and adds words to array list
         Scanner filScanner = new Scanner(wordBank);
         while(filScanner.hasNextLine()){
             words.add(filScanner.nextLine());
         }
+        //randomly selects word from list
         Random rand = new Random();
         int i = rand.nextInt(words.size());
         selectedWord = words.get(i).toLowerCase();
+
+        //initialize display word with underscores
+        displayWord = new char[selectedWord.length()];
+
+        for(int j=0 ; j < selectedWord.length(); j++){
+            displayWord[j]='_';
+        }
         filScanner.close();
     }
 
-    public void displayGame(){
-        System.out.println("Hangman Game Started!");
-        System.out.println("Guess the word: " + "_ ".repeat(selectedWord.length()));
-
+    //method displays game status and takes player input
+    public void displayGame()
+    {
+        System.out.println("Learning Hangman Game Started!");
+        System.out.println("Catgeory: Fruits");
+        System.out.println("Word Length: " + selectedWord.length());
         Scanner input = new Scanner(System.in);
-        while(winner ==false)
-        {
-            System.out.print("Enter your guess: ");
-            String guess = input.nextLine().toLowerCase();
 
-            if(selectedWord.equals(guess)){
-                System.out.println("Congratulations! You guessed the word: " + selectedWord);
-                winner = true;
-            } else {
-            System.out.println("incorrect guess. Try again!");
-            }
+        //game continues until player wins or runs out of lives
+        while(winner ==false && gameOver ==false)
+        {
+            System.out.println("Lives: " + lives);
+            System.out.println("Guess the word: " + String.valueOf(displayWord));
+            System.out.print("Enter a letter: ");
+            //takes only first character(even if player enters multiple characters)
+            // of input and converts to lowercase
+            char guess = input.next().toLowerCase().charAt(0);
+            System.out.println();
+            wordGuess(guess);
+            checkWin();
         }
         input.close();
-
     }
+
+    //method checks if guessed letter is in selected word
+    //updates display word and lives accordingly
+    //if wrong letter is guessed, calls checkLoss method
+    public void wordGuess(char guess)
+    {
+        boolean found = false;
+        if(guesses.contains(guess))
+        {
+            System.out.println("You already guessed that letter! Try again!");
+        }
+        else
+        {
+            for(int i = 0; i < selectedWord.length(); i++)
+            {
+                if(selectedWord.charAt(i) == guess)
+                {
+                    displayWord[i] = guess;
+                    found = true;
+                }
+            }
+                if(found == false)
+                {
+                    checkLoss();
+                    if(!gameOver)
+                    {
+                       System.out.println("Wrong guess! Try again!");
+                    }
+                }
+                else
+                {
+                    System.out.println("Correct guess! Keep going!");
+                }
+                guesses.add(guess);
+            }
+            System.out.println("Guesses: " + guesses);
+        }
+    
+    //method checks if player has guessed the entire word
+    public void checkWin()
+    {
+        if(String.valueOf(displayWord).equals(selectedWord))
+        {
+            winner = true;
+            System.out.println("Congratulations! You guessed the word: " + selectedWord);
+        }
+    }
+
+    //method reduces lives by 1 and checks if lives have reached 0
+    public void checkLoss()
+    {
+        lives = lives - 1;
+        if(lives==0)
+        {
+            gameOver = true;
+            System.out.println("Game Over! The word was: " + selectedWord);
+        }
+    }
+    
 }
+    
